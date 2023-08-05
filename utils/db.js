07b1bcd5c0b8b49891142e3d18df83728f59e7b6
nicolas ngwira <ngwiranicolas@gmail.com>
2023-08-05 09:48:02 +0200

    added error handling for db connection

--- utils/db.js
+++ utils/db.js
@@ -4,6 +4,8 @@ const mongoose=require("mongoose");
 const connection=mongoose.connect(process.env.mongoUrl,{
     useNewUrlParser:true,
     useUnifiedTopology:true
-});
+})
+.then((data)=>console.log(data))
+.catch((err)=>console.log(err));
 
 module.exports=connection;
